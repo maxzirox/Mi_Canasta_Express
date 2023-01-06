3c8d20aebfa2cd050109e238d6547b439a3c4f43
Maxi Guzman <32882558+maxzirox@users.noreply.github.com>
2023-01-06 13:28:09 -0300

    add full responsive design
    add full responsive design and rrss links in navbar

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -33,18 +33,39 @@ function Cards() {
   return (
     <>
     <h2 className='Hcard'>SURPRISE BREAKFAST</h2>
+    <Container fluid>
     <Swiper
-      spaceBetween={10}
-      slidesPerView={5}
+      breakpoints={{
+        385: {
+          width: 385,
+          slidesPerView: 2
+        },
+        446: {
+          width: 446,
+          slidesPerView: 2,
+        },
+        // when window width is >= 640px
+        640: {
+          width: 640,
+          slidesPerView: 3,
+        },
+        // when window width is >= 768px
+        987: {
+          width: 987,
+          slidesPerView: 5,
+        },
+      }}
+      spaceBetween={1}
+      slidesPerView={1}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
       { info.map((data) =>{
 
         return(
-          <Container fluid>
-          <SwiperSlide>
-          <Card key={data.id} style={{ textAlign: 'center', width: '12rem', height: '18rem'}}>
+          
+          <SwiperSlide key={data.id}>
+          <Card  style={{ textAlign: 'center', width: '12rem', height: '18rem'}}>
           <Card.Img variant="top" src={data.imagen} style={{ width: '7rem', height: '7rem', alignSelf: 'center'}} />
           <Card.Body>
             <Card.Text>{data.titulo}</Card.Text>
@@ -55,10 +76,11 @@ function Cards() {
           </Card.Body>
         </Card>
         </SwiperSlide>
-        </Container>
+        
       )})
       }
     </Swiper>
+    </Container>
     </>
   );
 }
