3ab62780a1927bc1702be1b0c504fb948d9b06c7
Maxi Guzman <32882558+maxzirox@users.noreply.github.com>
2023-01-14 13:50:55 -0300

    add detail || fixed navbar || best logic

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -1,89 +1,84 @@
-import Card from 'react-bootstrap/Card';
-import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import CardGroup from 'react-bootstrap/CardGroup';
+import { Card } from 'flowbite-react';
 import { useState, useEffect } from 'react';
-import desayunos from '../../Utils/DesayunosMocks';
-import Carousel from 'react-bootstrap/Carousel';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 import 'swiper/css';
 
-
 function Cards(products) {
-  const [info, setInfo] = useState([]);
+	const [info, setInfo] = useState([]);
 
-  const getBreakFast = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() =>{
-        console.log(products)
-        resolve(products)
-      }, 2000)
-    })
-  }
+	const getBreakFast = () => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve(products);
+			}, 2000);
+		});
+	};
 
-  useEffect( () => {
-   getBreakFast()
-   .then((res) => {
-    setInfo(res.products)
+	useEffect(() => {
+		getBreakFast().then((res) => {
+			setInfo(res.products);
+		});
+	}, []);
 
-   }) 
-    
-  }, [])
-
-
-  return (
-    
-    <Container fluid style={{marginBottom: '150px'}}>
-    <Swiper
-      breakpoints={{
-        385: {
-          width: 385,
-          slidesPerView: 2
-        },
-        446: {
-          width: 446,
-          slidesPerView: 2,
-        },
-        // when window width is >= 640px
-        640: {
-          width: 640,
-          slidesPerView: 3,
-        },
-        // when window width is >= 768px
-        987: {
-          width: 987,
-          slidesPerView: 5,
-        },
-      }}
-      spaceBetween={1}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      { info.map((data) =>{
-
-        return(
-          
-          <SwiperSlide key={data.id}>
-          <Card  style={{ textAlign: 'center', width: '12rem', height: '18rem'}}>
-          <Card.Img variant="top" src={data.imagen} style={{ width: '7rem', height: '7rem', alignSelf: 'center'}} />
-          <Card.Body>
-            <Card.Text>{data.titulo}</Card.Text>
-            <Card.Text>
-              ${data.precio}
-            </Card.Text>
-            <Button variant="outline-primary" style={{alignContent: 'center'}}>buy now</Button>
-          </Card.Body>
-        </Card>
-        </SwiperSlide>
-        
-      )})
-      }
-    </Swiper>
-    </Container>
-    
-  );
+	return (
+		<Swiper
+			breakpoints={{
+				385: {
+					width: 385,
+					slidesPerView: 2,
+				},
+				446: {
+					width: 446,
+					slidesPerView: 2,
+				},
+				// when window width is >= 640px
+				640: {
+					width: 640,
+					slidesPerView: 3,
+				},
+				// when window width is >= 768px
+				987: {
+					width: 987,
+					slidesPerView: 5,
+				},
+			}}
+			spaceBetween={1}
+			slidesPerView={1}
+			onSlideChange={() => console.log('slide change')}
+			onSwiper={(swiper) => console.log(swiper)}
+		>
+			{info.map((data) => {
+				return (
+					<SwiperSlide key={data.id}>
+						<div className='max-w-sm'>
+							<Card style={{ width: '200px', height: '350px' }}>
+								<img className='imgCard' src={data.imagen} alt={data.titulo} />
+								<a href='#'>
+									<h5 className='text-xl tracking-tight text-gray-900 dark:text-white text-center'>
+										{data.titulo}
+									</h5>
+								</a>
+								<h5 className='text-3xl text-gray-900 dark:text-white text-center'>
+									${data.precio}
+								</h5>
+								<div className='flex items-center justify-center'>
+								<Link to={`/detail/${data.titulo}`} style={ {textDecoration: 'none', color: 'aliceblue' } } >
+									<button
+										rel='noreferrer'
+										className='rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+									>
+										Detalles
+									</button>
+								</Link>
+								</div>
+							</Card>
+						</div>
+					</SwiperSlide>
+				);
+			})}
+		</Swiper>
+	);
 }
 
 export default Cards;
